dc6bb25c2e84572ab330250d6f4de935ee10cc03
imhuhan <imhuhan@bytedance.com>
2019-05-31 14:59:16 +0800

    practice of structs

---
+++ Structs/main.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	// alex := person{firstName: "Alex", lastName: "Anderson"}
+	// fmt.Println(alex)
+	// var alex person
+
+	// alex.firstName = "Alex"
+	// alex.lastName = "Anderson"
+
+	// fmt.Println(alex)
+	// fmt.Printf("%+v", alex)
+
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	// jimPointer := &jim
+	jim.updateName("jimmy")
+	jim.printInfo()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) printInfo() {
+	fmt.Printf("%+v", p)
+}
