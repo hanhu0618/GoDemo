f310dc5db8c5102a69fc54796430f1fa0d91705d
imhuhan <imhuhan@bytedance.com>
2019-05-31 17:01:21 +0800

    basic syntax of interface

---
+++ interface/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+// func printGreeting(eb englishBot) {
+// 	fmt.Println(eb.getGreeting())
+// }
+
+// func printGreeting(sb spanishBot) {
+// 	fmt.Println(sb.getGreeting())
+// }
+
+func (englishBot) getGreeting() string {
+	//VERY custom logic for generating an english greeting
+	return "Hi There!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
